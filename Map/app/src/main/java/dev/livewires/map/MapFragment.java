a527c9ae088feab80c44187a386337b5798184b2
James Doc <jamesdoc@gmail.com>
2016-07-31 21:58:01 +0100

    Add basic map

---
+++ Map/app/src/main/java/dev/livewires/map/MapFragment.java
@@ -0,0 +1,16 @@
+package dev.livewires.map;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapFragment extends SupportMapFragment {
+    /*
+     implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnMapLongClickListener,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMarkerClickListener
+     */
+}
